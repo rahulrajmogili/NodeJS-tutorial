188971bbf2fc196e5b899f0de33b936deb18ee0f
Rahul Raj Mogili <mogilirahulraj@gmail.com>
2021-05-24 10:48:51 -0400

    alternative types of exporting modules

--- 2-modules.js
+++ 2-modules.js
@@ -1,7 +1,11 @@
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
 
-sayHi('Susan')
-sayHi(names.john)
-sayHi(names.peter)
+// when you import a module, you actually invoke the functions in it
+require('./7-mind-grenade')
 
+// sayHi('Susan')
+// sayHi(names.john)
+// sayHi(names.peter)
+// console.log(data)
