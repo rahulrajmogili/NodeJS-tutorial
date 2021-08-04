7fde05f23c47867db07f792dbb53ff40f48ea5c0
Rahul Raj Mogili <mogilirahulraj@gmail.com>
2021-08-04 12:46:39 -0400

    modified some parts of repo

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -5,19 +5,21 @@ readFile("./content/first.txt", "utf-8", (err, result) => {
     return console.log(err);
   }
   const first = result;
+
   readFile("./content/second.txt", "utf-8", (err, result) => {
     if (err) {
       return console.log(err);
     }
+
     const second = result;
+
     writeFile(
       "./content/result-async.txt",
-      `Here is the result: ${first}, ${second}`,
+      `This is a combined text ${first} ${second}`,
       (err, result) => {
         if (err) {
           return console.log(err);
         }
-        console.log(result);
       }
     );
   });
